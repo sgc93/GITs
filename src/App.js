4768ea772fc1391ae97550c25445363b9f4dfa3d
sgc93 <smachewgedefaw@gmail.com>
2023-11-29 03:15:03 -0900

    retrieve HomePage component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,10 @@
 import "./App.css";
-import logo from "./assets/gits.png";
+import HomePage from "./Pages/HomePage/HomePage";
 
 function App() {
 	return (
 		<div className="App">
-			<header className="App-header">
-				<img src={logo} className="App-logo" alt="logo" />
-				<p>
-					Working with <code>GitHub API</code> and ReactJS.
-				</p>
-				<a
-					className="App-link"
-					href="https://reactjs.org"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					Figma Design
-				</a>
-			</header>
+			<HomePage />
 		</div>
 	);
 }
