9fb2475de681a8d62df0a1b53b8bc756f1f248ab
sgc93 <smachewgedefaw@gmail.com>
2023-12-02 13:30:24 -0900

    handle searching : fetch user lists based on query

--- src/Pages/SearchPage/Search.js
+++ src/Pages/SearchPage/Search.js
@@ -1,14 +1,41 @@
+import { useEffect, useState } from "react";
+import UserProfileCard from "../../Utils/UserProfileCard";
 import stars from "../../assets/stars.png";
 import "./Search.css";
 
 function Search({ query }) {
+	const [users, setUsers] = useState([]);
+	useEffect(
+		function () {
+			async function fetchUsers() {
+				const response = await fetch(
+					`https://api.github.com/search/users?q=${query}`
+				);
+				const data = await response.json();
+				setUsers(data.items);
+				console.log(data);
+			}
+
+			fetchUsers();
+		},
+		[query]
+	);
+
 	return (
 		<div className="app__search">
 			<div className="app__search-bg">
 				<img src={stars} alt="search bg" />
 			</div>
 			<div className="app__search-overlay">
-				<div className="app__search-results">Search Results For {query}</div>
+				<p>
+					Search Results For <span>{query}</span>
+				</p>
+				<div className="app__search-results">
+					{users.map((user) => (
+						<UserProfileCard user={user} />
+					))}
+					<UserProfileCard />
+				</div>
 			</div>
 		</div>
 	);
