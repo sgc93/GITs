5990d160b3dcf690001c835fea9d004107e449bf
sgc93 <smachewgedefaw@gmail.com>
2023-12-02 04:53:11 -0900

    handle scrolling while focus

--- src/componets/SearchBar.js
+++ src/componets/SearchBar.js
@@ -1,11 +1,13 @@
-import { useRef } from "react";
+import { useRef, useState } from "react";
 import search from "../assets/search.svg";
 import "./Component.css";
 
 function SearchBar() {
+	const [focused, setFocused] = useState(false);
 	const inputRef = useRef(null);
 
 	const handleInputFocus = () => {
+		setFocused(true);
 		window.scrollTo({
 			top: 1080,
 			behavior: "smooth",
@@ -13,7 +15,13 @@ function SearchBar() {
 	};
 
 	return (
-		<div className="app__searchbar">
+		<div
+			className={
+				focused
+					? "app__searchbar-focused app__searchbar"
+					: "app__searchbar app__searchbar-normal"
+			}
+		>
 			<input
 				type="text"
 				placeholder="Search or jump to ..."
