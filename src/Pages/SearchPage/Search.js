34170fc07a8823ab145d31a20b9f013b06937cbd
sgc93 <smachewgedefaw@gmail.com>
2023-12-02 04:54:11 -0900

    update layout while display search results

--- src/Pages/SearchPage/Search.js
+++ src/Pages/SearchPage/Search.js
@@ -1,15 +1,13 @@
 import stars from "../../assets/stars.png";
-import SearchBar from "../../componets/SearchBar";
 import "./Search.css";
 
-function Search() {
+function Search({ onFocus }) {
 	return (
 		<div className="app__search">
 			<div className="app__search-bg">
 				<img src={stars} alt="search bg" />
 			</div>
 			<div className="app__search-overlay">
-				<SearchBar />
 				<div className="app__search-results">Search Results</div>
 			</div>
 		</div>
