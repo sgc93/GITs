7983ad0977588aa5d74c0842aa378f20134b0c74
sgc93 <smachewgedefaw@gmail.com>
2023-12-02 11:31:52 -0900

    connect with searchbar

--- src/Pages/SearchPage/Search.js
+++ src/Pages/SearchPage/Search.js
@@ -1,14 +1,14 @@
 import stars from "../../assets/stars.png";
 import "./Search.css";
 
-function Search({ onFocus }) {
+function Search({ query }) {
 	return (
 		<div className="app__search">
 			<div className="app__search-bg">
 				<img src={stars} alt="search bg" />
 			</div>
 			<div className="app__search-overlay">
-				<div className="app__search-results">Search Results</div>
+				<div className="app__search-results">Search Results For {query}</div>
 			</div>
 		</div>
 	);
