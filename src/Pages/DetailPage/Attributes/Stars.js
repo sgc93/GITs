10738eaf4c0f64f0c2a72aabc5d740490e1624be
sgc93 <smachewgedefaw@gmail.com>
2023-12-11 02:21:49 -0900

    style repo owners in starred repos

---
+++ src/Pages/DetailPage/Attributes/Stars.js
@@ -0,0 +1,65 @@
+import { useEffect, useState } from "react";
+import { FaExternalLinkAlt, FaRegStar } from "react-icons/fa";
+
+function Repo({ repo }) {
+	return (
+		<div className="repo">
+			<div className="half">
+				<p className="name">{repo.name}</p>
+				<div className="owner">
+					<p className="owner-name">{repo.owner.login}</p>
+					<img src={repo.owner.avatar_url} alt="owner" />
+				</div>
+			</div>
+			<div className="half">
+				<a href={repo.html_url}>
+					<FaExternalLinkAlt />
+				</a>
+				<div className="stars">
+					<FaRegStar />
+					<p>{repo.stargazers_count}</p>
+				</div>
+			</div>
+		</div>
+	);
+}
+
+function Stars({ user }) {
+	const [repos, setRepos] = useState([]);
+	const [page, setPage] = useState(1);
+
+	useEffect(
+		function () {
+			async function fetchStarredRepos() {
+				const response = await fetch(
+					`https://api.github.com/users/${user.login}/starred`
+				);
+				const data = await response.json();
+				setRepos((repos) => data);
+				console.log(data);
+			}
+
+			fetchStarredRepos();
+		},
+		[user.login]
+	);
+
+	function handleSeeMore() {
+		setPage((page) => page + 1);
+	}
+
+	return (
+		repos && (
+			<>
+				<div className="user__repos">
+					{repos.map((repo) => (
+						<Repo key={repo.id} repo={repo} />
+					))}
+				</div>
+				<button onClick={() => handleSeeMore}>See More</button>
+			</>
+		)
+	);
+}
+
+export default Stars;
