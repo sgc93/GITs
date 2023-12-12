6eea3953e52ee228fe9443e4d6ed7664488e80d9
sgc93 <smachewgedefaw@gmail.com>
2023-12-12 01:21:11 -0900

    handle fetching and displaying list of top users

--- src/Pages/HomePage/Users/TopUser.js
+++ src/Pages/HomePage/Users/TopUser.js
@@ -1,6 +1,42 @@
+import { useEffect, useState } from "react";
 import indicator from "../../../assets/user_indicator.png";
 import "./TopUser.css";
+
+function User({ user }) {
+	return (
+		<div className="top">
+			<div className="list-rank">1</div>
+			<div className="list-data">
+				<img src={indicator} alt="user" />
+				<div>
+					<p className="name">Indicator</p>
+					<p className="bio">asdf ;lkh asdf ;l;kja sdfsd; ;lkj</p>
+				</div>
+				<div className="repos">
+					<a href="jdl">*</a>
+					<p>1234 repositories</p>
+				</div>
+			</div>
+		</div>
+	);
+}
+
 function TopUser() {
+	const [topUsers, setTopUsers] = useState([]);
+
+	useEffect(function () {
+		async function fetchTopUsers() {
+			const response = await fetch(
+				"https://api.github.com/search/users?q=repos:%3E0&sort=repositories&order=desc&per_page=100"
+			);
+			const parsedData = await response.json();
+			setTopUsers((topUsers) => parsedData.items);
+			console.log(parsedData.items);
+		}
+
+		fetchTopUsers();
+	}, []);
+
 	return (
 		<div className="app__top">
 			<div className="app__top-head">
@@ -19,7 +55,11 @@ function TopUser() {
 				<div className="app__top-main_icon">
 					<img src={indicator} alt="repos" />
 				</div>
-				<div className="app__top-main_list">list</div>
+				<div className="app__top-main_list">
+					{topUsers.map((user) => (
+						<User user={user} />
+					))}
+				</div>
 			</div>
 			<div className="app__top-btn">
 				<button>See All</button>
