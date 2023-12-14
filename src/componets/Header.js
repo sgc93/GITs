f8d8f53d6b55d68485c9cc2f73815506c2c6e5d1
sgc93 <smachewgedefaw@gmail.com>
2023-12-14 01:14:26 -0900

    connect tops to header navigation

--- src/componets/Header.js
+++ src/componets/Header.js
@@ -13,9 +13,9 @@ function Nav() {
 	return (
 		<div className="nav">
 			<a href="#topUsers">_topUsers</a>
-			<a href="topRepos">_topRepositories</a>
-			<a href="topOrgs">_topOrganizations</a>
-			<a href="Contact"> Contact()</a>
+			<a href="#topRepos">_topRepositories</a>
+			<a href="#topOrgs">_topOrganizations</a>
+			<a href="#Contact"> Contact()</a>
 		</div>
 	);
 }
